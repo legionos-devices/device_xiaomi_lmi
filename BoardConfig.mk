f06976aefcafa93b616cc9e3a65982fb86dc326f
RealSchnedi <schnedi3@gmail.com>
2021-03-12 23:20:15 +0100

    lmi: prebuilt: Update dtb/dtbo/kernel from MIUI 12.2.5.0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,3 @@
-
 # Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -135,8 +134,7 @@ TARGET_USES_INTERACTION_BOOST := true
 # Prebuilt Core
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-BOARD_PREBUILT_DTBIMAGE_DIR := $(DEVICE_PATH)/prebuilt
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 262144 
